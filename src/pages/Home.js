6ba53a9a36dfd8137ef362ccc80abd518d7e5e03
hakan.basturknewinn.com <forxynet@gmail.com>
2024-10-08 14:10:12 +0300

    updated home page with navigate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products');
+  }
+
   return (
     <div>
       <h1>My Home Page</h1>
       <p>Go to <Link to="/products"> the list of products</Link>.</p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </div>
   )
 }
